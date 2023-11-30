34296b82953f80cb25e70f127568dfeb7eb99cea
Courdevelops <courdevelops@gmail.com>
2023-11-30 14:14:03 -0500

    ready for menu to slide in

--- menuburger.js
+++ menuburger.js
@@ -12,11 +12,16 @@ let hamburger_close = document.getElementById('hamburger_close');
 // query the courdevs small logo
 let courdevs_top_logo = document.getElementById('courdevs_top_logo');
 
+// query the whole page
+let whole_page = document.getElementById('whole_page');
+
 
 // event listener on the hamburger
 burger_menu_button.addEventListener('click', function () {
     // test print
     console.log("Hamburger Menu Button");
+    // lock the whole page
+    // darken the whole page
     // toggle burger dissapear/re-appear
     top_hamburger.classList.toggle('dissapear');
     // toggle x appear/dissapear
@@ -24,5 +29,6 @@ burger_menu_button.addEventListener('click', function () {
     // toggle top logo appear/dissapear
     courdevs_top_logo.classList.toggle('appear');
     // toggle menu slide in/slide out
+    
 });
 
