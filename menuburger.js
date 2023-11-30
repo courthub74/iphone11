7aeefb59bb5f4d079e1329c88ab15c13e52c2452
Courdevelops <courdevelops@gmail.com>
2023-11-30 15:47:19 -0500

    nav works sweet

--- menuburger.js
+++ menuburger.js
@@ -14,20 +14,23 @@ let slide_in_menu = document.getElementById('slide_in_menu');
 let courdevs_top_logo = document.getElementById('courdevs_top_logo');
 
 // query the whole page
-let whole_page = document.querySelector('.container');
+let whole_page = document.getElementById('whole_page');
 
 // query the whole body
 let whole_body = document.querySelector('body');
+
+// query the you in the floating nav
+let you_thing = document.getElementById('you');
 
 
 // event listener on the hamburger
 burger_menu_button.addEventListener('click', function () {
     // test print
     console.log("Hamburger Menu Button");
-    // lock the whole page
+    // lock the whole body
     whole_body.classList.toggle('lock');
     // darken the whole page below the menu
-    // whole_page.classList.toggle('darken');
+    whole_page.classList.toggle('darken');
     // toggle burger dissapear/re-appear
     top_hamburger.classList.toggle('dissapear');
     // toggle x appear/dissapear
@@ -36,5 +39,7 @@ burger_menu_button.addEventListener('click', function () {
     courdevs_top_logo.classList.toggle('appear');
     // toggle menu slide in/slide out
     slide_in_menu.classList.toggle('slide-in')
+    // make floating nav dissapear
+    you_thing.classList.toggle('dissapear');
 });
 
