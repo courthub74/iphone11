67e0df59e62cb985276bb9e87faef6c3af963fc4
Courdevelops <courdevelops@gmail.com>
2023-12-02 15:13:43 -0500

    app 2 slide in done

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,8 @@
 // query the whole page
 let whole_page_portfolio = document.querySelector('body');
 
+// APP 1 SLIDE IN
+
 // query the app1content
 let app1content = document.getElementById('app1content');
 
@@ -27,4 +29,32 @@ app1back.addEventListener('click', function () {
     // slide the appcontent in (change the class)
     app1content.classList.toggle('slide_in');
 });
+
+// APP 2 SLIDE IN
+
+// query the app1content
+let app2content = document.getElementById('app2content');
+
+// query the app1 button
+let app2button = document.getElementById('app_2');
+
+// query the back arrow
+let app2back = document.getElementById('app2_back_arrow');
+
+// add event listener to app 1 button
+app2button.addEventListener('click', function () {
+    // freeze whole page
+    whole_page_portfolio.classList.toggle('lock');
+    // slide the appcontent in (change the class)
+    app2content.classList.toggle('slide_in');
+});
+
+// add event listener to app 1 back button
+app2back.addEventListener('click', function () {
+    // freeze whole page
+    whole_page_portfolio.classList.toggle('lock');
+    // slide the appcontent in (change the class)
+    app2content.classList.toggle('slide_in');
+});
+
 
