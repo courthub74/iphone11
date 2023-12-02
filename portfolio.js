29e253a2a582e0f64d2c3176b888867739612858
Courdevelops <courdevelops@gmail.com>
2023-12-02 15:07:51 -0500

    portfolio 1 slide in works

--- portfolio.js
+++ portfolio.js
@@ -7,10 +7,24 @@ let app1content = document.getElementById('app1content');
 // query the app1 button
 let app1button = document.getElementById('app_1');
 
+// query the back arrow
+let app1back = document.getElementById('app1_back_arrow');
+
 // add event listener to app 1 button
 app1button.addEventListener('click', function () {
     // test print
     console.log("APP 1 content button");
     // freeze whole page
     whole_page_portfolio.classList.toggle('lock');
+    // slide the appcontent in (change the class)
+    app1content.classList.toggle('slide_in');
 });
+
+// add event listener to app 1 back button
+app1back.addEventListener('click', function () {
+    // freeze whole page
+    whole_page_portfolio.classList.toggle('lock');
+    // slide the appcontent in (change the class)
+    app1content.classList.toggle('slide_in');
+});
+
